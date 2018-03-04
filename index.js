895a7f8e18d0f2224be164bce7bf3ea156ff0c2d
Peter Prib <peter.prib@gmail.com>
2018-03-04 15:54:10 +1100

    fix a few bugs - still doesn't issue
    in the main works

--- index.js
+++ index.js
@@ -18,11 +18,15 @@ const path = require('path')
 	,express = require('express');
 function browserSetup(app,p) {
 	console.log('browserSetup');
-	var f,include,type,n
-		,s="(function() {console.log('initialise');"
+	var f,include,type,n,post=""
+		,s="console.log('initialise');"
 			+"\nfunction Loading (f) {this.callOnLoaded=f;this.count=1"
-			+	"\nthis.finished=function () {this.count--\nalert('loading='+this.count);"
-						+"\nif(this.count==0 & this.callOnLoaded) {this.callOnLoaded.call;}"
+			+	"\nthis.finished=function () {"
+						+"\nconsole.log('initialise waiting '+loading.count)"
+						+"\nif(--loading.count>0) return;"
+						+"\nconsole.log('initialise post load calls')"
+						+"\nif(loading.callOnLoaded) {loading.callOnLoaded.call};"
+						+"\npostScript.call(window);"
 			+	"\n}\n}"
 			+"\nvar loading=new Loading();\nhead0=document.getElementsByTagName('head')[0];"
 			;
@@ -40,28 +44,75 @@ function browserSetup(app,p) {
 		}
 		console.log("use "+n.id +' ==> '+f);
 		app.use('/'+n.id, express.static(f));
+		if(!n.file & !n.include) {
+			n.include=[require(f+'/package.json').main];
+			console.log("adding include "+n.include);
+		}
+		if(n.file) {
+			if(n.post) {
+				post+="\nvar e=document.createElement('script');"
+					+"\ne.type='text/javascript';"
+					+"\ne.src = '"+n.id+"';"
+					+"\nhead0.appendChild(e);\nconsole.log('initialise include issued for: "+n.id+"');";
+					;
+			} else {
+				s+="\nloading.count++;"
+					+"\nvar e=document.createElement('script');"
+					+"\ne.type='text/javascript';"
+					+"\ne.src = '"+n.id+"';"
+					+"\ne.onload = loading.finished;"
+					+"\nhead0.appendChild(e);\nconsole.log('initialise include issued for: "+n.id+"');";
+					;
+			}
+			if(n.post) {
+			} else {
+				s+="\nhead0.appendChild(e);console.log('initialise include issued for: "+include+"');";
+			}
+
+		}
 		if(n.include) {
 			for(var j in n.include) {
 				include=n.id+'/'+n.include[j];
 				if((include.match(/[^\\\/]\.([^.\\\/]+)$/) || [null]).pop()==='js') {
-					s+="\nloading.count++;"
-						+"\nvar e=document.createElement('script');"
-						+"\ne.type='text/javascript';"
-						+"\ne.src = '"+include+"';";
-						+"\ne.onLoad = loading.finished;"; 
+					if(n.post) {
+						post+="\nvar e=document.createElement('script');"
+							+"\ne.type='text/javascript';"
+							+"\ne.src = '"+include+"';"
+							;
+					} else {
+						s+="\nloading.count++;"
+							+"\nvar e=document.createElement('script');"
+							+"\ne.type='text/javascript';"
+							+"\ne.src = '"+include+"';"
+							+"\ne.onload = loading.finished;"
+							;
+					}
 				} else {
-					s+="\nloading.count++;"
-						+"\nvar e=document.createElement('link');"
-						+"\ne.type='text/javascript';"
-						+"\ne.rel='stylesheet';"
-						+"\ne.href='"+include+"';" 
-						+"\ne.onLoad = loading.finished;"; 
+					// no onload for link
+					if(n.post) {
+						post+="\nvar e=document.createElement('link');"
+							+"\ne.type='text/javascript';"
+							+"\ne.rel='stylesheet';"
+							+"\ne.href='"+include+"';"
+							; 
+					} else {
+						s+="\nvar e=document.createElement('link');"
+							+"\ne.type='text/javascript';"
+							+"\ne.rel='stylesheet';"
+							+"\ne.href='"+include+"';" 
+							;
+					}
 				}
-				s+="\nhead0.appendChild(e);console.log('initialise include issued for: "+include+"');";
+				if(n.post) {
+					post+="\nhead0.appendChild(e);console.log('initialise include issued for: "+include+"');";
+				} else {
+					s+="\nhead0.appendChild(e);console.log('initialise include issued for: "+include+"');";
+				}
 			}
 		}
 	}
-	s+="\nloading.finished();\nconsole.log('initialise end');})()";
+	s+="function postScript(){"+post+"\nconsole.log('initialise post load completed')}";
+	s+="\nconsole.log('initialise waiting count '+loading.count)\nloading.finished();\nconsole.log('initialise end');";
 	app.get('/initialise',function (req, res, next) {
 		res.send(s); 
 	});
